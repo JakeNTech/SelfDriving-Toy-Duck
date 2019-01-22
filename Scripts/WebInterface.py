b8a2bf2efa583b00812889d318e048e9186d5816
JakeNTech <36766388+JakeNTech@users.noreply.github.com>
2019-01-22 19:21:24 +0000

    Bodge Job

--- Scripts/WebInterface.py
+++ Scripts/WebInterface.py
@@ -1,5 +1,4 @@
 #--------------------
-import DuckCamera
 # Imports the camerefeed from the DuckCamera script
 import Utilitys
 #Import the utilitys script
@@ -14,7 +13,56 @@ from tornado.ioloop import PeriodicCallback
 import tornado.websocket
 import tornado.web
 # Imports Tornado Modules
+class DuckCamera(object):
+	def __init__(self,_parameters):
+		Utils.print_log("Init. Camera")
 
+		self.stream = True
+		self.path = _parameters['path']
+		#Tells other scrips about the camerea
+		self.picam = PiCamera()
+		self.picam.rotation = 180
+		self.picam.framerate = 15
+		self.picam.resoloution = (_parameters['width'], _parameters['height'])
+		Camera.rotation = 180
+		# This sets the rotation and resoloutin of the camera.
+		time.sleep(2)
+		#this allows for the camera to sort its self out in tearms of expouser
+		#and contrast, and fous. This also factors in the delays
+		def save_frame(self,_turn):
+			name = string(Utils.ms_ephoc())+":"+string(_turn)
+			#This takes the movement name ie.left and converts it to text
+			#This allows for training
+
+#Getting the camera feed
+#NO TWITCH
+class CameraStream(object):
+	def __init__(self,_duck):
+		self.duck = _duck
+		self.camera = self.duck.camera
+		thread = threading.Thred(target=self.run, args=())
+		thread.deamon = True
+		thread.start()
+		#This creates a thread for the camera feed
+	def run(self):
+		self.detection = ObjectDetection()
+		while (self.camera.stream):
+			raw_capture = picamera.array.PiRGBArray(self.camera.picam)
+			self.camera.picam.capture(raw_capture, format="rgb",use_video_port=True)
+			#this creates a RGB capture and files it into an array
+
+			img = self.detection.detect(raw_capture.array.astype('unit8'))
+			# this runs the object detection
+
+			image_array = numpy.zeros([1,230,330,3])
+			image_array[0]=img
+			self.camera.last_img = image_array
+			#Saves the current image/frame being used and displayed
+			
+			img = Image.fromarray(img.astype("unit8"),mode="RBG")
+			f = io.BtyesIO()
+			img.save(f, "JPEG")
+			self.camera.last_img_bytes = f.getvalue()
 class LocalServer():
 	#Class to help contain the code
 	def __init__(self,params,_duck):
