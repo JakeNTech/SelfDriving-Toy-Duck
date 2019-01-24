4c161730001a8ba5d7ce931a76b1876d5272e278
JakeNTech <36766388+JakeNTech@users.noreply.github.com>
2019-01-24 14:25:40 +0000

    Update UserClient.js

--- WebServer/UserClient.js
+++ WebServer/UserClient.js
@@ -56,9 +56,9 @@ var client = {
 		};
 		// to prevent oddness with a video stream on the users device, the video feed is converted too images, and this ensures
 		// that there is not issues with this
-		this.socket.onmessage = function(messsageEvent){
-			video.scr = "data:image/jpeg;base64," + messageEvent.data;
-		};
+        this.socket.onmessage = function (messageEvent) {
+            video.src = "data:image/jpeg;base64," + messageEvent.data;
+        };
 	},
 	move: function(_direction){
 		console.log('Move '+_direction)
