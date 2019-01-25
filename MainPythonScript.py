e7353c36b89dfae5603c92435d64a539e20ca244
JakeNTech <36766388+JakeNTech@users.noreply.github.com>
2019-01-25 17:37:04 +0000

    Update MainPythonScript.py

--- MainPythonScript.py
+++ MainPythonScript.py
@@ -7,17 +7,22 @@ from Scripts import Utilities , WebInterface, DrivingDuck
 #This is the script that ties is all togeter
 
 if(__name__ == "__main__"):
+	print("Starting")
 	arguments = Utilities.get_arguments()
+	print("Got Arguments")
 	# This gets the arguments from the Utilities script
 	#print(arguments)
 	parameters = Utilities.get_parameters(arguments.train)
 	#Featches the perameaters
+	print("Got parameters")
 	#print(parameters)
 	duck = DrivingDuck.DrivingDuck(parameters)
 	# This gets the duck ready for ation
+	print("Duck is ready!")
 	
 	duck.WebInterface = WebInterface.LocalServer(parameters['webserver_parameters'], duck)
 	# This starts to host the GUI
+	print("Starting GUI")
 
 	print('Quack! Ready to get the bread!')
 	duck.WebInterface.stream()
