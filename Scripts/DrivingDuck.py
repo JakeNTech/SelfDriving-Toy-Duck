9e35c8c54a08242119014ab22c6a2b74d824763f
JakeNTech <36766388+JakeNTech@users.noreply.github.com>
2019-01-25 17:57:29 +0000

    Update DrivingDuck.py

--- Scripts/DrivingDuck.py
+++ Scripts/DrivingDuck.py
@@ -23,7 +23,7 @@ class DrivingDuck(object):
 		self.width = _parameters['duck_parameters']['width']
 		self.height = _parameters['duck_parameters']['height']
 		self.verbose = _parameters['duck_parameters']['verbose']
-		self.configuration = _parameters['duck_parameters']['duck_parameters']
+		self.configuration = _parameters['duck_parameters']['duck_configuration']
 		self.channels = _parameters['duck_parameters']['channels']
 		#Initailises all the parameatres 
 	#-----------Training Mode----------------
