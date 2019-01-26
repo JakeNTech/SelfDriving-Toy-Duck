b4875fb1b00e03c185934aa08a0c1c7ece0fcd5b
JakeNTech <36766388+JakeNTech@users.noreply.github.com>
2019-01-26 20:09:18 +0000

    Update DuckHead.py

--- Scripts/DuckHead.py
+++ Scripts/DuckHead.py
@@ -16,7 +16,7 @@ from Scripts import Utilities
 class DuckHead(object):
 	def __init__(self,_parameters):
 		Utilities.log("Initialisation model")
-		self.model = keras.models.load_model(_parameters['model'],custom_objects=None,compile=False)
+		self.model = pickle.loads(_parameters['model'])
 		self.graph = tensorflow.get_default_graph()
 		#Set the parameaters for the following script
 	def GetDirections(self,_image):
